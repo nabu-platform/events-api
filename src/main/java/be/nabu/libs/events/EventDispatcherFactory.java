553d9947c5ca2b2f7656f8c5867ba0b64f8ec39e
alex <alex@nabu.be>
2014-11-15 16:13:03 +0100

    Added custom service support

--- src/main/java/be/nabu/libs/events/EventDispatcherFactory.java
+++ src/main/java/be/nabu/libs/events/EventDispatcherFactory.java
@@ -1,6 +1,9 @@
 package be.nabu.libs.events;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Iterator;
+import java.util.List;
 import java.util.ServiceLoader;
 
 import be.nabu.libs.events.api.EventDispatcher;
@@ -17,14 +20,44 @@ public class EventDispatcherFactory {
 	
 	private EventDispatcher eventDispatcher;
 	
+	@SuppressWarnings("unchecked")
 	public EventDispatcher getEventDispatcher() {
 		if (eventDispatcher == null) {
-			ServiceLoader<EventDispatcher> serviceLoader = ServiceLoader.load(EventDispatcher.class);
-			Iterator<EventDispatcher> iterator = serviceLoader.iterator();
-			if (iterator.hasNext())
-				eventDispatcher = iterator.next();
-			else
-				eventDispatcher = new EventDispatcherStub();
+			try {
+				// let's try this with custom service loading based on a configuration
+				Class<?> clazz = Thread.currentThread().getContextClassLoader().loadClass("be.nabu.utils.services.ServiceLoader");
+				Method declaredMethod = clazz.getDeclaredMethod("load", Class.class);
+				List<EventDispatcher> list = (List<EventDispatcher>) declaredMethod.invoke(null, EventDispatcher.class);
+				if (!list.isEmpty()) {
+					eventDispatcher = list.get(0);
+				}
+			}
+			catch (ClassNotFoundException e) {
+				// ignore, the framework is not present
+			}
+			catch (NoSuchMethodException e) {
+				// corrupt framework?
+				throw new RuntimeException(e);
+			}
+			catch (SecurityException e) {
+				throw new RuntimeException(e);
+			}
+			catch (IllegalAccessException e) {
+				// ignore
+			}
+			catch (InvocationTargetException e) {
+				// ignore
+			}
+			if (eventDispatcher == null) {
+				ServiceLoader<EventDispatcher> serviceLoader = ServiceLoader.load(EventDispatcher.class);
+				Iterator<EventDispatcher> iterator = serviceLoader.iterator();
+				if (iterator.hasNext()) {
+					eventDispatcher = iterator.next();
+				}
+				else {
+					eventDispatcher = new EventDispatcherStub();
+				}
+			}
 		}
 		return eventDispatcher;
 	}
